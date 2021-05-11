801f7892b1f81f9f31fd9d2a9ecf2a605c9570a0
Thuận Nguyễn Văn <thuan2609.tk1@gmail.com>
2021-05-11 10:25:52 +0700

    Create BASE Models, Ignore APIKEY

---
+++ SCPlayer/SCPlayer/Managers/APIServices.swift
@@ -0,0 +1,37 @@
+//
+//  APIServices.swift
+//  SCPlayer
+//
+//  Created by Thuận Nguyễn Văn on 11/05/2021.
+//
+
+import Foundation
+
+final class APIServices {
+    
+    static let shared = APIServices()
+    
+    public func fetchTracksJSON(completion: @escaping (Result<[Track], Error>) -> ()) {
+        let urlString = URLString.tracks.url
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            //Failed
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            //Sucsess
+            do {
+                guard let data = data else { return }
+                let courses = try JSONDecoder().decode([Track].self, from: data)
+                completion(.success(courses))
+            } catch let jsonError {
+                completion(.failure(jsonError))
+            }
+        }.resume()
+    }
+}
+
+
---
+++ SCPlayer/SCPlayer/Managers/URLString.swift
@@ -0,0 +1,23 @@
+//
+//  URLString.swift
+//  SCPlayer
+//
+//  Created by Thuận Nguyễn Văn on 11/05/2021.
+//
+
+import Foundation
+
+let baseURL = "https://api.soundcloud.com"
+let clientID = "7c8ae3eed403b61716254856c4155475"
+
+enum URLString {
+    case tracks
+    
+    var url: String {
+        switch self {
+        case .tracks:
+            return "\(baseURL)/tracks/?client_id=\(clientID)&limit=200"
+        }
+    }
+}
+
