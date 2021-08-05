1f72cf5df7d70f1ed5824fd27f7c17f926da4413
Thuận Nguyễn Văn <thuan2609.tk1@gmail.com>
2021-08-05 09:23:46 +0700

    Ref[TASK][1111] Unit Test

---
+++ SCPlayer/SCPlayerTests/SCPlayerTests.swift
@@ -0,0 +1,29 @@
+//
+//  SCPlayerTests.swift
+//  SCPlayerTests
+//
+//  Created by Thuận Nguyễn Văn on 05/08/2021.
+//
+
+import XCTest
+
+class SCPlayerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+       
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        measure {
+            
+        }
+    }
+
+}
